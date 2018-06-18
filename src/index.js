0521c05e26cb3f0de4617cb505438787c09d1cfa
Maxim Filimonov <tpaktopsp@gmail.com>
2018-06-18 16:04:07 +0800

    trying example from issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import app from './app';
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => process.stdout.write(`Running on :${port}\n`));
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+let currentApp = require('./app');
+
+const server = http.createServer(currentApp).listen(PORT, () => {
+  console.log(`Server started on: http://localhost:${PORT}`);
+});
 
 if (module.hot) {
-  module.hot.accept('./app');
+  module.hot.accept('./app', () => {
+    try {
+      server.removeListener('request', currentApp);
+      /* eslint-disable-next-line global-require */
+      currentApp = require('./app');
+      server.on('request', currentApp);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+  module.hot.accept();
+  module.hot.dispose(() => {
+    console.log('Server stopped');
+    server.close();
+  });
 }
